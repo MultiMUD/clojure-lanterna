a29469c243139ee2c655b0b15bc74a3806834114
Steve Losh <steve@stevelosh.com>
2012-09-07 14:03:27 -0400

    Rely on the latest version of lanterna.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://sjl.bitbucket.org/clojure-lanterna/"
   :license {:name "LGPL"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.googlecode.lanterna/lanterna "2.0.1"]]
+                 [com.googlecode.lanterna/lanterna "2.0.3"]]
   :java-source-paths ["./java"]
   ; :repositories {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"}
   )
