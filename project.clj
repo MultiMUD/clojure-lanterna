80b839e5f86c5688d46c0bf2c50bf17e89e2ba1f
Ryan Neufeld <ryan@rkn.io>
2013-06-11 20:21:16 -0400

    Upgrade Lanterna to 2.1.5 for redraw bugfix

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clojure-lanterna "0.9.3"
+(defproject clojure-lanterna "0.9.4"
   :description "A Clojure wrapper around the Lanterna terminal output library."
   :url "http://sjl.bitbucket.org/clojure-lanterna/"
   :license {:name "LGPL"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.googlecode.lanterna/lanterna "2.1.1"]]
+                 [com.googlecode.lanterna/lanterna "2.1.5"]]
   :java-source-paths ["./java"]
   ; :repositories {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"}
   )
