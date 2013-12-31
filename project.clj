85a3c3a19bc377d90810333706e8eb8658145050
Shawn Krisman <telaviv@github>
2013-12-30 23:23:56 -0800

    updated lanterna to 2.1.7 and clojure-lanterna to 0.9.5.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clojure-lanterna "0.9.4"
+(defproject clojure-lanterna "0.9.5"
   :description "A Clojure wrapper around the Lanterna terminal output library."
   :url "http://sjl.bitbucket.org/clojure-lanterna/"
   :license {:name "LGPL"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.googlecode.lanterna/lanterna "2.1.5"]]
+                 [com.googlecode.lanterna/lanterna "2.1.7"]]
   :java-source-paths ["./java"]
   ; :repositories {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"}
   )
