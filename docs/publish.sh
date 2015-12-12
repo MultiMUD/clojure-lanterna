e1a2d75d70f20a773371b30af827ad462d2d9341
Steve Losh <steve@stevelosh.com>
2015-12-12 13:34:55 +0000

    Update publish script

--- docs/publish.sh
+++ docs/publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf ./build
-~/lib/virtualenvs/d/bin/d
+d
 hg -R ~/src/sjl.bitbucket.org pull -u
 rsync --delete -a ./build/ ~/src/sjl.bitbucket.org/clojure-lanterna
 hg -R ~/src/sjl.bitbucket.org commit -Am 'clojure-lanterna: Update site.'
