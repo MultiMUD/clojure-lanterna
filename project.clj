fb8237aa15c0700b6fc9bfa1cd39be33271eae1f
MultiMUD's Benden Breghnowsky <benden@multimud.net>
2017-02-07 20:31:46 +0100

    clj 1.6 is the first clojure to not regard dash-prefixed methods in a protocol as fields, it seems. Quiets reflection warnings for the protocol methods not being found as fields.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A Clojure wrapper around the Lanterna terminal output library."
   :url "http://multimud.github.io/clojure-lanterna/"
   :license {:name "LGPL"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.googlecode.lanterna/lanterna "3.0.0-beta3"]]
   :java-source-paths ["./java"]
   ; :repositories {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"}
